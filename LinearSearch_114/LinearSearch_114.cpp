3052f5d35a6b28e48c4f97813065cccbe2ae47db
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-04-13 14:39:23 +0700

    Memasukkan data atau Element Array

--- LinearSearch_114/LinearSearch_114.cpp
+++ LinearSearch_114/LinearSearch_114.cpp
@@ -5,6 +5,19 @@ int arr[20];
 int n;
 int i;
 
+void input()
+{
+    while (true)
+    {
+        cout << "Enter the number of element in the Array : ";
+        cin >> n;
+        if ((n > 0) && (n <= 20))
+            break;
+        else
+            cout << "\nArray should have minimum 1 and maximum 20 element. \n\n";
+    }
+}
+
 
 
 int main()
